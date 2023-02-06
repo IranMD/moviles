0ac56767d427c9a9c98b3ca356bf16e1b09333cb
Iran <iranmendoza01@outlook.com>
2023-02-05 19:27:36 -0600

    prueba 1

--- App.js
+++ App.js
@@ -22,11 +22,33 @@ const config = {
 };
 
 // extend the theme
-export const theme = extendTheme({ config });
+export const theme = extendTheme({ colors: {
+  // Add new color
+  color: {
+    DARK_PRIMARY: '#29353C',
+    LIGHT_PRIMARY: '#AAC7D8',
+    PRIMARY: '#44576D',
+    300: '#7AC1E4',
+    400: '#47A9DA',
+    500: '#0088CC',
+    600: '#007AB8',
+    700: '#006BA1',
+    800: '#005885',
+    900: '#003F5E',
+  },
+  // Redefining only one shade, rest of the color will remain same.
+  amber: {
+    400: '#d97706',
+  },
+},
+config: {
+  // Changing initialColorMode to 'dark'
+  initialColorMode: 'dark',
+}, });
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <Center
         _dark={{ bg: "blueGray.900" }}
         _light={{ bg: "blueGray.50" }}
